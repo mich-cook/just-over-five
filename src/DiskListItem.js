0c7a617d3ac7dcdbf583d9b68b9093fdbd29b05c
mich <mich-cook@users.noreply.github.com>
2020-01-22 22:38:16 -0800

    remove currently unused programs variable until we get it implemented

--- src/DiskListItem.js
+++ src/DiskListItem.js
@@ -4,7 +4,7 @@ class DiskListItem extends Component {
 
   render() {
 	// allow each one to have a default instead of anything-matches object
-    const { title = "No title provided", programs = [ "No programs provided"], blocksFree = 664 } = this.props.info;
+    const { title = "No title provided", blocksFree = 664 } = this.props.info;
     const { collection = "Unspecified" } = this.props
 
     return (
