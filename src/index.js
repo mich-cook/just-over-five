b7c25b981349b636ffa8fa5f23f48353d76f04f9
mich <mich-cook@users.noreply.github.com>
2020-07-22 17:43:42 -0700

    removing deprecated AddDiskForm element from page

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<div>
     <Disks />
-    <AddDiskForm />
 </div>, document.getElementById('disks'));
 
 // If you want your app to work offline and load faster, you can change
