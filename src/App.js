ebcc65d3baf704eaf7bf718b5dadf24c6ec779c3
mich <mich-cook@users.noreply.github.com>
2020-10-10 14:13:27 -0700

    maintain user state in <App> and handle changes to user state. move logged in user stuff to /my so we can put a home at /

--- src/App.js
+++ src/App.js
@@ -1,15 +1,50 @@
 import React, { Component } from 'react';
 import { Router, navigate } from '@reach/router';
 
+import firebase from './Firebase.js';
+
 import Login from './Login.js';
 import MyDisks from './Disks.js';
 
 export default class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      "user": null,
+      "displayName": null,
+      "uid": null,
+      "userPhoto": null
+    };
+  }
+
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({
+          "user": user,
+          "displayName": user.displayName,
+          "uid": user.uid,
+          "userPhoto": user.photoURL
+        });
+        navigate('/my');
+      } else {
+        this.setState({
+          "user": null,
+          "displayName": null,
+          "uid": null,
+          "userPhoto": null
+        });
+      }
+    });
+  }
+
   render() {
     return (
       <Router>
         <Login path="/login" />
         <MyDisks path="/" />
+        <MyDisks path="/my" />
       </Router>
     );
   }
