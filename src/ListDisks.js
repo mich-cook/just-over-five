7a0c4aeb3174f0434d109a7b2f5124dfcd515403
mich <mich-cook@users.noreply.github.com>
2020-10-11 00:20:05 -0700

    now that <ListDisks> has a uid to make the request, handle it properly to display the collection

--- src/ListDisks.js
+++ src/ListDisks.js
@@ -6,18 +6,39 @@ class ListDisks extends Component {
     super(props);
 
     this.state = {
-      disks: []
+      "uid": null,
+      "disks": []
     };
   }
 
-  componentDidMount() {
+  fetchUserDiskInfo() {
+    // if we don't have a user, don't bother doing the fetch
+    if (this.props.user === null)  return;
+
+    // break the loop caused by updating the data
+    // then the update causing componentDidUpdate() to run
+    // which then updates the data which then ...
+    if (this.props.user === this.state.uid) return;
+
     fetch(`//localhost:8000/api/v1/c64/user/${this.props.user}`)
       .then(response => response.json())
       .then(data => {
-        if (data !== null) {
-          this.setState({ "disks": data.disks });
-        }  // else leave state.disks as []
+        let state = { "uid": this.props.user };
+        if (data !== null)  state.disks = data.disks;
+        this.setState(state);
       });
+  }
+
+  componentDidMount() {
+    // firebase usually hasn't figured this out on load
+    // when the component is mounted, but try just in case
+    this.fetchUserDiskInfo();
+  }
+
+  componentDidUpdate() {
+    // typically we find out that a user is logged in
+    // after the component mounted.
+    this.fetchUserDiskInfo();
   }
 
   render() {
@@ -25,7 +46,7 @@ class ListDisks extends Component {
       <ul>
         {this.state.disks.map(item => (
           <li key={item._id} onClick={e => this.props.showListing(item._id)}>
-            <span className="title">{item.name}</span> | 
+            <span className="title">{item.title}</span> |
             <span className="blocks-free"> {item.free}</span>
           </li>
         ))}
